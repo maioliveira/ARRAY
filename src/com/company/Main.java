a73c8b592ea25db27a1366c2376d0b1466ea6df8
Mai Oliveira <maiza.oliveira54@gmail.com>
2020-03-16 19:06:50 -0300

    int, double e char

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,22 +11,122 @@ public class Main {
 	// write your code here
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] arrayFruta = new char[10];
-        char arraylength = (char) arrayFruta.length;
+    /*double
+
+        double[] arrayNumerico = new double[10];
+        double arrayLength = arrayNumerico.length;
+        double contador = 0;
+
+        arrayNumerico[0] = Double.parseDouble("1");
+        arrayNumerico[1] = Double.parseDouble("2");
+        arrayNumerico[2] = Double.parseDouble("3");
+        arrayNumerico[3] = Double.parseDouble("4");
+        arrayNumerico[4] = Double.parseDouble("5");
+        arrayNumerico[5] = Double.parseDouble("6");
+        arrayNumerico[6] = Double.parseDouble("7");
+        arrayNumerico[7] = Double.parseDouble("8");
+        arrayNumerico[8] = Double.parseDouble("9");
+        arrayNumerico[9] = Double.parseDouble("10");
+
+        System.out.println(arrayNumerico[0]);
+        System.out.println(arrayNumerico[1]);
+        System.out.println(arrayNumerico[2]);
+        System.out.println(arrayNumerico[3]);
+        System.out.println(arrayNumerico[4]);
+        System.out.println(arrayNumerico[5]);
+        System.out.println(arrayNumerico[6]);
+        System.out.println(arrayNumerico[7]);
+        System.out.println(arrayNumerico[8]);
+        System.out.println(arrayNumerico[9]);
+*/
+    //String - int
+
+        String[] arrayFruta = new String[10];
+        int arraylength = arrayFruta.length;
         int contador = 0;
 
-        arrayFruta[0] = "Morango";
-        arrayFruta[1] = "Abacate";
-        arrayFruta[2] = "Banana";
-        arrayFruta[3] = "Laranja";
-        arrayFruta[4] = "Maracujá";
-        arrayFruta[5] = "Limão";
-        arrayFruta[6] = "Mamão";
-        arrayFruta[7] = "Manga";
-        arrayFruta[8] = "Uva";
+        arrayFruta[0] = "Maracujá";
+        arrayFruta[1] = "Manga";
+        arrayFruta[2] = "Mamão";
+        arrayFruta[3] = "Limão";
+        arrayFruta[4] = "Laranja";
+        arrayFruta[5] = "Uva";
+        arrayFruta[6] = "Morango";
+        arrayFruta[7] = "Jaca";
+        arrayFruta[8] = "Goiaba";
         arrayFruta[9] = "Abacaxi";
 
         System.out.println(arrayFruta[0]);
+        System.out.println(arrayFruta[1]);
+        System.out.println(arrayFruta[2]);
+        System.out.println(arrayFruta[3]);
+        System.out.println(arrayFruta[4]);
+        System.out.println(arrayFruta[5]);
+        System.out.println(arrayFruta[6]);
+        System.out.println(arrayFruta[7]);
+        System.out.println(arrayFruta[8]);
+        System.out.println(arrayFruta[9]);
+
+        /*char
+        char[] arrayAlfabeto = new char[26];
+        char arrayLength = (char) arrayAlfabeto.length;
+        int contador = 0;
+
+        arrayAlfabeto[0] = 'A';
+        arrayAlfabeto[1] = 'B';
+        arrayAlfabeto[2] = 'C';
+        arrayAlfabeto[3] = 'D';
+        arrayAlfabeto[4] = 'E';
+        arrayAlfabeto[5] = 'F';
+        arrayAlfabeto[6] = 'G';
+        arrayAlfabeto[7] = 'H';
+        arrayAlfabeto[8] = 'I';
+        arrayAlfabeto[9] = 'J';
+        arrayAlfabeto[10] = 'K';
+        arrayAlfabeto[11] = 'L';
+        arrayAlfabeto[12] = 'M';
+        arrayAlfabeto[13] = 'N';
+        arrayAlfabeto[14] = 'O';
+        arrayAlfabeto[15] = 'P';
+        arrayAlfabeto[16] = 'Q';
+        arrayAlfabeto[17] = 'R';
+        arrayAlfabeto[18] = 'S';
+        arrayAlfabeto[19] = 'T';
+        arrayAlfabeto[20] = 'U';
+        arrayAlfabeto[21] = 'V';
+        arrayAlfabeto[22] = 'W';
+        arrayAlfabeto[23] = 'X';
+        arrayAlfabeto[24] = 'Y';
+        arrayAlfabeto[25] = 'Z';
+
+
+        System.out.println(arrayAlfabeto[0]);
+        System.out.println(arrayAlfabeto[1]);
+        System.out.println(arrayAlfabeto[2]);
+        System.out.println(arrayAlfabeto[3]);
+        System.out.println(arrayAlfabeto[4]);
+        System.out.println(arrayAlfabeto[5]);
+        System.out.println(arrayAlfabeto[6]);
+        System.out.println(arrayAlfabeto[7]);
+        System.out.println(arrayAlfabeto[8]);
+        System.out.println(arrayAlfabeto[9]);
+        System.out.println(arrayAlfabeto[10]);
+        System.out.println(arrayAlfabeto[11]);
+        System.out.println(arrayAlfabeto[12]);
+        System.out.println(arrayAlfabeto[13]);
+        System.out.println(arrayAlfabeto[14]);
+        System.out.println(arrayAlfabeto[15]);
+        System.out.println(arrayAlfabeto[16]);
+        System.out.println(arrayAlfabeto[17]);
+        System.out.println(arrayAlfabeto[18]);
+        System.out.println(arrayAlfabeto[19]);
+        System.out.println(arrayAlfabeto[20]);
+        System.out.println(arrayAlfabeto[21]);
+        System.out.println(arrayAlfabeto[22]);
+        System.out.println(arrayAlfabeto[23]);
+        System.out.println(arrayAlfabeto[24]);
+        System.out.println(arrayAlfabeto[25]);
+*/
 
         /*while (contador < arraylength) {
             System.out.println(arrayFruta (contador+1));
